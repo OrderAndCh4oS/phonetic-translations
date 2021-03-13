00f46147cf764b0d061dd59b41eabf1120587ba3
Sarcoma <sarcoma@live.co.uk>
2021-03-13 17:46:32 +0000

    Add response time

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const textEl = document.getElementById('text');
 const resultEl = document.getElementById('result');
+const responseTimeEl = document.getElementById('response-time');
 const languageEl = document.getElementById('language');
 const submitEl = document.getElementById('submit');
 
 submitEl.addEventListener('click', async () => {
     const text = textEl.value;
-    if(!text) alert('Please enter some text')
+    if(!text) alert('Please enter some text');
+    const t0 = performance.now()
     const response = await fetch(`https://9k24oe3gyg.execute-api.eu-west-2.amazonaws.com/prod/translate/${languageEl.value}`, {
         method: 'post',
         headers: {
@@ -14,6 +16,8 @@ submitEl.addEventListener('click', async () => {
         },
         body: JSON.stringify({text})
     });
+    const t1 = performance.now()
+    responseTimeEl.innerText = (t1 - t0).toFixed(2);
     console.log(response);
     const result = await response.json()
     console.log(result);
